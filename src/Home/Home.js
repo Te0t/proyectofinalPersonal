1c1ec6e09f7ef617e53a4111a16c499a4e8c4783
407 <407@academicos.cesde.edu.co>
2022-11-29 16:37:18 -0500

    se crea proyecto react

---
+++ src/Home/Home.js
@@ -0,0 +1,47 @@
+import './Home.css'
+export function Home() {
+
+    return (
+        <>
+            <section>
+                <div class="banner">
+
+                </div>
+            </section>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
+                            <h1>David Guetta</h1>
+                            <p>Pierre David Guetta​​​ es un DJ, compositor y productor francés. Actualmente, ocupa el puesto #2 según la encuesta realizada por DJ Magazine.​Se inició como tal en varias discotecas durante las décadas de 1980 y de 1990, antes de fundar Gum Productions y lanzar su primer álbum: Just a Little More Love.</p>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/cinemaharley.appspot.com/o/perfilDavidguetta.jpg?alt=media&token=048135ed-28fe-4d1b-a8c2-6f09a50f1e00" alt="perfil david guetta" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4 text-end">
+                            <h1>Conciertos</h1>
+                            <i class="bi bi-vinyl-fill fs-1">Med</i>
+                            <i class="bi bi-vinyl-fill fs-1">Med</i>
+                            <i class="bi bi-vinyl-fill fs-1">Med</i>
+                            <i class="bi bi-vinyl-fill fs-1">Med</i>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/cinemaharley.appspot.com/o/noticiasDavidguetta.webp?alt=media&token=f1ec97b6-e428-444f-8f56-002fc29be384" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/cinemaharley.appspot.com/o/sliderDavidguetta.jpg?alt=media&token=86758729-f53f-48f8-9b8a-3cd1a89733dc" alt="" class="img-fluid w-100"/>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/cinemaharley.appspot.com/o/asieraDavidguetta.jpg?alt=media&token=f3f6f25f-0d94-4dd6-bd87-1f3145108146" alt="" class="img-fluid w-100"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
